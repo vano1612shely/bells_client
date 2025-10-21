420441be5dcc485c8364f9b702535d312012044e
Ivan Shelengivskiy <vano1612shely@gmail.com>
2025-10-21 21:58:08 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import path from 'path'
-// https://vitejs.dev/config/
+
 export default defineConfig({
   base: '/',
   plugins: [
