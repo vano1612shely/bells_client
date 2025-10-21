bf27701291f7f36f4c0f7042efd09af300704441
Ivan Shelengivskiy <vano1612shely@gmail.com>
2025-10-21 21:53:07 +0300

    update favicon and title

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import path from "path"
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
     viteReact(),
